179f55ea61017b4c2fa22d960c38f95d201b27bd
Daniel Hüsken <mail@danielhuesken.de>
2008-07-03 05:12:42 +0000

    WP 2.6 Path copatibilty

--- minimeta-widget.php
+++ minimeta-widget.php
@@ -84,6 +84,7 @@ Change log:
                            adminlink generation improfments
                            more <ul> xhtml fixes
    Version 3.5.5    Fixes for K2 1.0-RC6
+                          WP 2.6 Plugin Path copatibilty
 */
  
 //Display Widget 
@@ -453,10 +454,10 @@ function widget_minimeta_wp_head_login() {
 //WP-Head hooks low Priority
 function widget_minimeta_wp_head() {
     //Set Style sheet
-    if (file_exists('wp-content/plugins/'.dirname(plugin_basename(__FILE__)).'/custom/minimeta-widget.css')) {
-        echo "<link rel=\"stylesheet\" href=\"".get_bloginfo('wpurl')."/wp-content/plugins/".dirname(plugin_basename(__FILE__))."/custom/minimeta-widget.css\" type=\"text/css\" media=\"screen\" />";
+    if (file_exists(WP_CONTENT_DIR.'/plugins/'.dirname(plugin_basename(__FILE__)).'/custom/minimeta-widget.css')) {
+        echo "<link rel=\"stylesheet\" href=\"".WP_CONTENT_URL."/plugins/".dirname(plugin_basename(__FILE__))."/custom/minimeta-widget.css\" type=\"text/css\" media=\"screen\" />";
     } elseif (file_exists('wp-content/plugins/'.dirname(plugin_basename(__FILE__)).'/minimeta-widget.css')) {
-        echo "<link rel=\"stylesheet\" href=\"".get_bloginfo('wpurl')."/wp-content/plugins/".dirname(plugin_basename(__FILE__))."/minimeta-widget.css\" type=\"text/css\" media=\"screen\" />";
+        echo "<link rel=\"stylesheet\" href=\"".WP_CONTENT_URL."/plugins/".dirname(plugin_basename(__FILE__))."/minimeta-widget.css\" type=\"text/css\" media=\"screen\" />";
     }
 }
 
@@ -545,8 +546,15 @@ function widget_minimeta_register_WP() {
 // add all action and so on only if plugin loaded.
 function widget_minimeta_init() {
     global $wp_version,$pagenow;
-	//Loads language files
-	load_plugin_textdomain('MiniMetaWidget', 'wp-content/plugins/'.dirname(plugin_basename(__FILE__)).'/lang');
+	
+    // Pre-2.6 compatibility
+    if ( !defined('WP_CONTENT_URL') )
+        define( 'WP_CONTENT_URL', get_option('siteurl') . '/wp-content');
+    if ( !defined('WP_CONTENT_DIR') )
+        define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
+
+    //Loads language files
+	load_plugin_textdomain('MiniMetaWidget', WP_CONTENT_DIR.'/plugins/'.dirname(plugin_basename(__FILE__)).'/lang');
 	
     // Let only Activate on WordPress Version 2.5 or heiger
     if (version_compare($wp_version, '2.5', '<')) {
